f3b6ee4be49ef621dd0cec2ba045dae9d32554e3
Lucas <lucas.hartridge@cloudbeds.com>
2022-11-08 16:00:31 +0100

    fix: update prettier and eslit

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import Title from 'components/title';
-import styles from './styles.module.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import Title from 'components/title'
+import styles from './styles.module.css'
 
 export interface Props {
-  url: string;
-  theme?: 'light' | 'dark';
+  url: string
+  theme?: 'light' | 'dark'
 }
 
 export function Docco(props: Props) {
   return (
-    <div className={props.theme == "light" ? styles.container : styles.dark}>
+    <div className={props.theme == 'light' ? styles.container : styles.dark}>
       <h3>URL: {props.url}</h3>
       <h6>NAME: {props.theme ?? props.theme}</h6>
-      <Title title="Docco rocks" />
+      <Title title='Docco rocks' />
     </div>
   )
 }
 
 export function init(url: string, element: HTMLElement) {
-  const root = ReactDOM.createRoot(element);
-  root.render(Docco({ url }));
+  const root = ReactDOM.createRoot(element)
+  root.render(Docco({ url }))
 }
