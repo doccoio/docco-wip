7690ad1ed0c7cece88f90b0078ca78ca4af0a480
José Fonte <jose.fonte@cloudbeds.com>
2022-11-09 11:00:05 +0000

    feat: fix lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Title from 'components/title'
-import styles from './styles.module.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Title from 'components/title';
+import styles from './styles.module.css';
 
 export interface Props {
   url: string;
