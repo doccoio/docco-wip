c0cd1fe5178beddf076283a8b600603c993a5763
Lucas <lucas.hartridge@cloudbeds.com>
2022-11-09 09:51:05 +0100

    fix: improve eslint file to use neccesary dependencies, add release it and conventional commits
    Conventional commits was added to the package.json, eslint configuration has been updated to use
    minimal dependencies, add back .stories to lint them

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Title from 'components/title';
+import Title from './components/title';
 import styles from './styles.module.css';
 
 export interface Props {
@@ -13,7 +13,7 @@ export function Docco(props: Props) {
     <div className={props.theme === 'light' ? styles.container : styles.dark}>
       <h3>URL: {props.url}</h3>
       <h6>NAME: {props.theme ?? props.theme}</h6>
-      <Title title="Docco rocks" />
+      <Title title='Docco rocks' />
     </div>
   );
 }
